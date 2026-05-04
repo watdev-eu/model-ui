d34b279bd8ff760928426ca2b46a8b43abd609bf
Rik Oudega <me@rikben.nl>
2026-05-04 18:12:34 +0300

    MCA authentication bug solved

--- src/api/mca_preset_active.php
+++ src/api/mca_preset_active.php
@@ -13,15 +13,6 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
     session_start();
 }
 
-if (!Auth::isLoggedIn()) {
-    http_response_code(401);
-    echo json_encode([
-        'ok' => false,
-        'error' => 'You must be logged in.',
-    ]);
-    exit;
-}
-
 if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
     http_response_code(405);
     echo json_encode(['ok' => false, 'error' => 'Method not allowed']);
